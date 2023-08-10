c2cfd1befad994249a0290f9fb18f9801bcb4cbf
Pavan281 <53928214+Pavan281@users.noreply.github.com>
2023-08-10 19:25:18 +0530

    Add files via upload

---
+++ apps/result/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.forms import modelformset_factory
+
+from apps.corecode.models import AcademicSession, AcademicTerm, Subject
+
+from .models import Result
+
+
+class CreateResults(forms.Form):
+    session = forms.ModelChoiceField(queryset=AcademicSession.objects.all())
+    term = forms.ModelChoiceField(queryset=AcademicTerm.objects.all())
+    subjects = forms.ModelMultipleChoiceField(
+        queryset=Subject.objects.all(), widget=forms.CheckboxSelectMultiple
+    )
+    remarks = forms.CharField(label='remarks',max_length=100, empty_value='')
+
+
+EditResults = modelformset_factory(
+    Result, fields=("test_score", "exam_score"), extra=0, can_delete=True,
+
+)
